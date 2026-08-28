1a66c6aaabb1ec995d8bd76a8b39a081472aeaed
ruben-9091 <ruben_9091@hotmail.com>
2026-08-28 23:17:04 +0200

    feat: cleaning pages

--- web/gluco-fit/src/components/ui/index.jsx
+++ web/gluco-fit/src/components/ui/index.jsx
@@ -1,2 +1,3 @@
 export { default as NavBar } from "./navbar/navbar"; 
 export { default as ParticlesBackground } from "./particles-background/particles-background"; 
+export { default as Jumbotron } from "./jumbotron/jumbotron"; 
